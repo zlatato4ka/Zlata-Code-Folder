8db7f28b25ee102088065b24bc44abe122a9f8ac
zlatato4ka <pinchcf14@gmail.com>
2024-09-16 08:55:52 -0600

    friday

---
+++ 1-Demos and lessons/Lesson4Demo/src/App.java
@@ -0,0 +1,19 @@
+// Project name: Lesson 4 Demo
+// Name: Zlata Kapustyan
+// Date: 13 Sept 2024
+// Description: Going over comments and special characters 
+/* lalalalala 
+ * lalala
+ * allala
+ * lala
+*/
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Wor\\ld!");
+        System.out.println("\tHow was your day going?");
+        System.out.println("Its \"cloudy\" with a chance of meatballs today");
+    }
+}
