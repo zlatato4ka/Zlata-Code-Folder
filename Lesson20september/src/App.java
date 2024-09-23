c3cd0682e7f9a91e5db154716b14aef4c8e7efc9
zlatato4ka <pinchcf14@gmail.com>
2024-09-23 08:52:11 -0600

    Work from friday 20th sept

---
+++ Lesson20september/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double totalMoney = 4.75; // Total amount of money you have 
+        double wingCost = 0.75; // Cost of one chicken wing 
+         // Calculate the number of wings you can buy 
+                int numberOfWings = (int) (totalMoney / wingCost); 
+                // Output the result
+                 System.out.println("You can buy " + numberOfWings + " chicken wings."); }
+        
+    
+    
+}
