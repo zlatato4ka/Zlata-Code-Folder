346e95f319b1cf447156c0342b3b8b7da370f5e6
zlatato4ka <pinchcf14@gmail.com>
2024-09-23 08:56:32 -0600

    Friday 20th september assignment 6

--- Lesson20september/src/App.java
+++ Lesson20september/src/App.java
@@ -1,3 +1,8 @@
+//Program name: Chiken wings
+//Student name: Zlata Kapustyan
+//Date: 20 September 2024
+//Program Description: counting amount of chiken you can buy
+
 public class App {
     public static void main(String[] args) throws Exception {
         double totalMoney = 4.75; // Total amount of money you have 
