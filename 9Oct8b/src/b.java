de55032677a282eafc8fb53d0e6c54e3032898bc
zlatato4ka <pinchcf14@gmail.com>
2024-10-09 09:29:56 -0600

    assignment  8b 9 october

---
+++ 9Oct8b/src/b.java
@@ -0,0 +1,18 @@
+class BaseClass {
+    public void baseMethod() {
+        System.out.println("This is the base class method.");
+    }
+}
+
+// Subclass
+class MySubclass extends BaseClass {
+    // Method 1
+    public void methodOne() {
+        System.out.println("This is method one in the subclass.");
+    }
+
+    // Method 2
+    public void methodTwo() {
+        System.out.println("This is method two in the subclass.");
+    }
+}
---
+++ 9Oct8b/src/runner.java
@@ -0,0 +1,23 @@
+//BaseClass
+//Zlata Kapustyan
+// ocotober 9 2024
+//Create a sub class with two methods
+//Create a runner class that calls those methods 
+//and shows an output.
+public class runner {
+    public static void main(String[] args) {
+        // Create an instance of the subclass
+        MySubclass myObject = new MySubclass();
+
+        // Call methods from the subclass
+        myObject.methodOne();
+        myObject.methodTwo();
+    }          
+}
+
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+        
+//     }
+// }
