5459dae650b6ac4fefd16886112514ee68d1ac84
zlatato4ka <pinchcf14@gmail.com>
2024-10-15 09:25:21 -0600

    assignment 10

--- a10assignment/src/SumRunner.java
+++ a10assignment/src/SumRunner.java
@@ -1,3 +1,20 @@
+// Program name: SumRunner
+// Student name: Zlata Kapustyan
+// Date: 15 September 2024
+//Program Description:Given two numbers, calculate the sum and print the sum. sum = one + two sum is equal to the value of one added to the value of two
+
+//             In this assignment I would like you to use Sum.java to create a method that you will pass parameters to the program SumRunner.java. 
+            
+//             You will write the code for the methods: setNums, Sum, and Print in Sum.java and instantiate the same methods in SumRunner.java. 
+            
+//             When you run SumRunner.java your program will add two numbers together. 
+            
+//             Please submit both Sum and SumRunner with additional test cases.
+
+ 
+
+
+
 import java.util.Scanner;
 
 public class SumRunner {
